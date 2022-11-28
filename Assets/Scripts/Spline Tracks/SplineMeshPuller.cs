2741a05f87e05be72b6be3f39d9b1627c2053c60
orbitusii <dlodholm@icloud.com>
2022-11-28 11:36:17 -0700

    Added collider generation to SplineMeshPuller

--- Assets/Scripts/Spline Tracks/SplineMeshPuller.cs
+++ Assets/Scripts/Spline Tracks/SplineMeshPuller.cs
@@ -8,11 +8,29 @@ public class SplineMeshPuller : MonoBehaviour
     public bool ForceBakeOnStart = true;
     public int MeshIndex = 0;
 
+    public bool GenerateColliders;
+    public int[] ColliderIndices;
+
     void Start()
     {
         if (ForceBakeOnStart) BakedSource?.Bake();
 
         (GetComponent(typeof(MeshFilter)) as MeshFilter).mesh = BakedSource?.Meshes[MeshIndex];
+
+        if(GenerateColliders)
+        {
+            Transform parent = this.transform;
+
+            foreach(int i in ColliderIndices)
+            {
+                Mesh m = BakedSource.Meshes[i];
+                GameObject newCol = new GameObject($"Collider {i}");
+                newCol.transform.parent = parent;
+
+                var meshCol = (MeshCollider)newCol.AddComponent(typeof(MeshCollider));
+                meshCol.sharedMesh = m;
+            }
+        }
     }
 
     // Update is called once per frame
